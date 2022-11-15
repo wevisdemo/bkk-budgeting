5897d7a95a220f9d8247c2555ced1d9d4df1d6d3
Nathakit Sae-Tan <nat@pupil-labs.com>
2022-11-16 03:00:18 +0700

    remove comments and styleResources

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,8 @@ import type { NuxtConfig } from "@nuxt/types";
 const BASE_PATH = process.env.BASE_PATH || "";
 
 const config: NuxtConfig = {
-  // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "budget",
     meta: [
@@ -18,15 +16,12 @@ const config: NuxtConfig = {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["@wevisdemo/ui/styles/typography.css", "@wevisdemo/ui/styles/components.css"],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: ["@nuxtjs/style-resources", "@nuxt/typescript-build"],
 
   build: {
@@ -36,10 +31,6 @@ const config: NuxtConfig = {
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/axios", "@nuxtjs/pwa"],
 
   extends: ["@nuxtjs/eslint-config-typescript"],
-
-  styleResources: {
-    scss: ["~/assets/styles/variables.scss"],
-  },
 
   publicRuntimeConfig: {
     path: {
@@ -52,13 +43,11 @@ const config: NuxtConfig = {
     base: BASE_PATH || "/",
   },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: "/",
   },
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       lang: "en",
