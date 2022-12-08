ebc360320178310121aa9226a04492881569dd4b
Nathakit Sae-Tan <nat@pupil-labs.com>
2022-12-09 00:57:37 +0700

    update persist problems component - responsive updated for mobile

---
+++ components/BoxContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="py-14 grid gap-4 sm:grid-6">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "BoxContainer",
+});
+</script>
--- components/PersistProblems.vue
+++ components/PersistProblems.vue
@@ -1,28 +1,66 @@
 <template>
-  <div class="py-14 px-20 grid gap-6">
-    <div class="wv-h8 text-center">อะไรที่ยังเป็นปัญหาที่ค้างคาใจคุณอยู่ทุกวันนี้</div>
-    <div class="grid grid-cols-4">
-      <div v-for="(item, index) in problems" :key="index" class="grid justify-center">
+  <BoxContainer>
+    <div class="wv-h8 sm:pb-12 text-center hidden sm:block">
+      {{ title }}
+    </div>
+    <div class="wv-h5 sm:pb-12 text-center block sm:hidden wv-font-bold">
+      {{ title }}
+    </div>
+    <p class="block sm:hidden text-center">เลือกได้ 1 ข้อ</p>
+    <!-- top -->
+    <div class="flex flex-wrap gap-4 md:gap-6 lg:gap-10 justify-center">
+      <div
+        v-for="(item, index) in problemsTop"
+        :key="index"
+        class="flex sm:flex-col bg-white sm:bg-inherit p-2 sm:p-0 rounded-md basis-full sm:basis-32 md:basis-40 flex-shrink-0 gap-4 sm:text-center items-center"
+      >
         <img
-          class="w-40 h-40"
+          class="w-10 h-10 sm:w-32 sm:h-32 md:w-40 md:h-40"
           :src="`${$config.path.images}/persistent-problems/${item.img}.png`"
           :alt="item.title"
         />
         <p class="wv-b4 wv-font-bold">{{ item.title }}</p>
       </div>
     </div>
-  </div>
+    <!-- bottom -->
+    <div class="flex flex-wrap gap-4 md:gap-6 lg:gap-10 justify-center">
+      <div
+        v-for="(item, index) in problemsBottom"
+        :key="index"
+        class="flex sm:flex-col bg-white sm:bg-inherit p-2 sm:p-0 rounded-md basis-full sm:basis-32 md:basis-40 flex-shrink-0 gap-4 sm:text-center items-center"
+      >
+        <img
+          class="w-10 h-10 sm:w-32 sm:h-32 md:w-40 md:h-40"
+          :src="`${$config.path.images}/persistent-problems/${item.img}.png`"
+          :alt="item.title"
+        />
+        <p class="wv-b4 wv-font-bold">{{ item.title }}</p>
+      </div>
+    </div>
+  </BoxContainer>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import BoxContainer from "~/components/BoxContainer.vue";
 import problemsData from "~/data/problems.json";
 
 export default Vue.extend({
   name: "AllocationQuestion",
+  components: { BoxContainer },
+  data() {
+    return {
+      title: "อะไรที่ยังเป็นปัญหาที่ค้างคาใจคุณอยู่ทุกวันนี้",
+    };
+  },
   computed: {
-    problems() {
-      return problemsData;
+    problemsTop() {
+      const topData = problemsData.slice(0, 4);
+      return topData;
+    },
+    problemsBottom() {
+      const topData = problemsData.slice(4, 7);
+      return topData;
     },
   },
 });
