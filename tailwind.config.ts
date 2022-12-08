fe41f62420b0870b138b19768579aed519235c8c
Nathakit Sae-Tan <nat@pupil-labs.com>
2022-12-09 01:40:29 +0700

    add cream 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ export default <Partial<Config>>{
       },
       backgroundColor: {
         "wv-cream": "#E8E4D8",
+        "wv-cream-2": "#D9D4C5",
         "wv-green": "#4CC35D",
       },
     },
