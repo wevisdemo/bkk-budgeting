cf900c49243b0d89dc3bca3b9b1cfd949ecf0a80
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-01-09 01:53:15 +0700

    remove placeholder

--- pages/index.vue
+++ pages/index.vue
@@ -5,53 +5,10 @@
       <div
         class="max-w-7xl w-full p-6 m-auto flex flex-col justify-center items-center"
       >
-        <!-- intro box -->
-        <div
-          class="sm:max-w-lg rounded-lg sm:bg-black sm:text-white pb-8 sm:py-14 sm:px-20 grid gap-6"
-        >
-          <p class="wv-b3 text-center">
-            ทุกวันนี้เมืองที่เราใช้ชีวิต
-            ยังมีหลายอย่างไม่ได้เป็นแบบที่เราวาดฝันไว้ใช่ไหม?
-          </p>
-          <p class="wv-b3 text-center">
-            เราเองในฐานะประชาชนผู้เสียภาษีทางตรงและทางอ้อม
-            ก็ควรได้ร่วมออกแบบการใช้งบประมาณของเมือง
-            เพื่อสร้างเมืองในแบบที่เราต้องการได้
-          </p>
-          <p class="wv-b3 text-center sm:text-wv-green font-bold">
-            ถ้างั้นขอชวนมาลองเริ่มต้นที่เมืองหลวงอย่างกรุงเทพมหานครกันก่อนเลย!!
-          </p>
-        </div>
-        <!-- issues -->
         <PersistProblems />
-        <!-- strategies -->
         <StrategicPlans />
         <DevelopmentTimeline />
-        <div class="py-14 px-20 grid gap-6">
-          <p class="wv-b5">Lorem ipsum</p>
-          <h5 class="wv-h5">Lorem ipsum title bold</h5>
-          <p class="wv-b5">Date</p>
-        </div>
-        <!-- text:  -->
-        <div class="py-14 px-20 grid gap-6">
-          <p class="wv-b5">Lorem ipsum</p>
-          <h5 class="wv-h5">Lorem ipsum title bold</h5>
-          <p class="wv-b5">Date</p>
-        </div>
-        <!-- timeline -->
-        <div class="py-14 px-20 grid gap-6">
-          <!-- todo -->
-        </div>
-        <!-- text: budget -->
-        <div class="py-14 px-20 grid gap-6">
-          <h2 class="wv-h1">Lorem ipsum title bold</h2>
-          <p class="wv-b5">Date</p>
-          <p class="wv-b5">budget</p>
-        </div>
-        <!-- viz -->
-        <div class="py-14 px-20 grid gap-6">
-          <!-- todo -->
-        </div>
+        <BudgetPlans />
         <AllocationQuestion />
       </div>
     </div>
@@ -64,6 +21,7 @@ import AllocationQuestion from "~/components/AllocateQuestion.vue";
 import DevelopmentTimeline from "~/components/DevelopmentTimeline.vue";
 import PersistProblems from "~/components/PersistProblems.vue";
 import StrategicPlans from "~/components/StrategicPlans.vue";
+import BudgetPlans from "~/components/budget/BudgetPlans.vue";
 import Landing from "~/components/landing/Landing.vue";
 
 export default Vue.extend({
@@ -74,6 +32,7 @@ export default Vue.extend({
     PersistProblems,
     StrategicPlans,
     DevelopmentTimeline,
+    BudgetPlans,
   },
   data() {
     return {
