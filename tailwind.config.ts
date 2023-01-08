45bdd4ebb46cdbb184c38af6ca546aeabad2131d
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-01-09 00:57:56 +0700

    add new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,15 @@ export default <Partial<Config>>{
     extend: {
       textColor: {
         "wv-green": "#4CC35D",
+        "wv-gray-3": "#DDDDDD",
+        "wv-gray-4": "#858585",
       },
       backgroundColor: {
         "wv-cream": "#E8E4D8",
         "wv-cream-2": "#D9D4C5",
         "wv-green": "#4CC35D",
         "wv-blue": "#3277FF",
+        "wv-gray-3": "#DDDDDD",
       },
     },
   },
