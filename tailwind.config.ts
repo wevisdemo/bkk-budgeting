33597f6205175ddaec23349603703e36465ea84c
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-01-08 21:08:54 +0700

    add new bg color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default <Partial<Config>>{
         "wv-cream": "#E8E4D8",
         "wv-cream-2": "#D9D4C5",
         "wv-green": "#4CC35D",
+        "wv-blue": "#3277FF",
       },
     },
   },
