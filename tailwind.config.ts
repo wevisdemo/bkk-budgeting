9fefa0e7a6b7a3f0a4050ab485053762b21f2c3e
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-01-24 01:51:26 +0700

    update tailwind config - update content - extend bg colors - add safelist for dynamic classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,17 @@ import type { Config } from "tailwindcss";
 // import defaultTheme from "tailwindcss/defaultTheme";
 
 export default <Partial<Config>>{
+  content: [
+    "./layout/**/*.{html,js,ts,vue}",
+    "./pages/**/*.{html,js,ts,vue}",
+    "./components/**/*.{html,js,ts,vue}",
+  ],
   theme: {
     extend: {
       textColor: {
         "wv-green": "#4CC35D",
         "wv-gray-3": "#DDDDDD",
-        "wv-gray-4": "#858585",
+        "wv-gray-5": "#858585",
       },
       backgroundColor: {
         "wv-cream": "#E8E4D8",
@@ -15,10 +20,27 @@ export default <Partial<Config>>{
         "wv-green": "#4CC35D",
         "wv-blue": "#3277FF",
         "wv-gray-3": "#DDDDDD",
+        "wv-gray-4": "#F3F3F3",
+        "wv-safe": "#3277FF",
+        "wv-environment": "#BACB37",
+        "wv-connectivity": "#FFC2F1",
+        "wv-democracy": "#FF7A00",
+        "wv-management": "#C462F2",
+        "wv-economic": "#FFB800",
+        "wv-equality": "#FF2828",
       },
       borderColor: {
         "wv-green": "#4CC35D",
       },
     },
   },
+  safelist: [
+    "bg-wv-safe",
+    "bg-wv-environment",
+    "bg-wv-connectivity",
+    "bg-wv-democracy",
+    "bg-wv-management",
+    "bg-wv-economic",
+    "bg-wv-equality",
+  ],
 };
