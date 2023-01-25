36c60c6547f9e540468c38703b598789625b6a7f
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-01-26 02:54:56 +0700

    add form dialog component

---
+++ components/dialog/FormDialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="fixed w-full h-full left-0 right-0 top-0 bottom-0 flex justify-center items-center z-50 bg-dialog"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "FormDialog",
+});
+</script>
+
+<style scoped>
+.bg-dialog {
+  background-color: rgba(0, 0, 0, 0.6);
+}
+</style>
