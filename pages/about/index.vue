803d79e2a2117ff11308b109030ea84f6b5462be
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-01 02:49:40 +0700

    add about page (wip)

---
+++ pages/about/index.vue
@@ -0,0 +1,203 @@
+<template>
+  <BoxContainer>
+    <div class="max-w-3xl m-auto flex flex-col gap-20">
+      <div class="text-center mt-10 mb-16">
+        <h1 class="wv-h1 wv-bold">เกี่ยวกับโครงการ</h1>
+      </div>
+      <!-- about -->
+      <div class="grid gap-6">
+        <h5 class="wv-h5 wv-bold">เกี่ยวกับโปรเจกต์นี้</h5>
+        <div class="grid gap-4">
+          <p>
+            ทุกวันนี้...เมืองที่อาศัยและใช้ชีวิตอยู่
+            หลายสิ่งหลายอย่างไม่ได้เป็นไปดั่งใจและวาดฝันไว้
+            ปัญหาหลายอย่างเกิดขึ้นในหลายพื้นที่ เช่น ปัญหาขยะ น้ำเน่าเสีย
+            ทางขึ้น-ลงสำหรับผู้พิการ ผู้สูงอายุ
+            แถมลักษณะปัญหาของแต่ละพื้นที่ยังแตกต่างกันอีก
+            คงจะดีประชาชนในฐานะผู้เสียภาษีทั้งทางตรงและทางอ้อม
+            สามารถมีส่วนในการร่วมออกแบบการใช้งบประมาณเพื่อพัฒนาเมืองของเราเองให้ดีขึ้นและเป็นไปอย่างได้ดั่งที่ต้องการได้
+          </p>
+          <p>
+            ในหลายประเทศเริ่มเปิดโอกาสให้ประชาชนมีส่วนร่วมกับงบประมาณได้ในรูปแบบที่เรียกว่า
+            “งบประมาณแบบมีส่วนร่วม” (Participatory Budgeting)
+            ซึ่งเป็นเครื่องมือในการมีส่วนร่วมที่ให้ประชาชนในชุมชนสามารถตัดสินเกี่ยวกับการใช้งบประมาณ
+          </p>
+          <p>
+            ทีม Punch Up , องค์กรต่อต้านคอร์รัปชัน (ประเทศ) , Glow Story , Hand Social
+            Enterprise , Good Society Thailand และ Social Integrity Architecture and
+            Mechanism Design Lab จึงตั้งใจพัฒนาแพลตฟอร์มนี้ในรูปแบบเทคโนโลยีภาคประชาชน
+            (Civic Tech) เพื่อเปิดเผยข้อมูลให้ทุกคนในฐานะผู้เสียภาษี ได้เข้าถึง เข้าใจ
+            และเป็นส่วนหนึ่งของการออกแบบงบประมาณของกรุงเทพมหานคร
+            โดยเริ่มจากคำถามที่อาจจะคาใจหลายๆ คนว่า
+            ตอนนี้กรุงเทพมหานครวางแผนในการแก้ปัญหาที่เกิดขึ้นอย่างไร?
+            ใช้งบประมาณเท่าไหร่? และในฐานะประชาชน
+            เราจะมีส่วนร่วมแสดงความคิดเห็นได้อย่างไรบ้าง?
+            โดยเราตั้งใจว่าจะนำการร่วมโหวตและความคิดเห็นนี้รวบรวมยื่นต่อผู้ว่าราชการจังหวัดกรุงเทพมหานครและหน่วยงานที่เกี่ยวข้องต่อไป
+          </p>
+          <p>
+            โปรเจกต์นี้นับเป็นโปรเจกต์ทดลองสร้างแพลตฟอร์ม “งบประมาณแบบมีส่วนร่วม”
+            ให้กับจังหวัด/เมืองต่างๆ ในประเทศไทย
+            โดยเริ่มจากเมืองหลวงอย่างกรุงเทพมหานครเป็นต้นแบบ แล้วพัฒนาไปสู่เมืองอื่นๆ
+            พร้อมอยากชวนทุกคนมามีส่วนร่วมในการพัฒนาสังคมให้ดีขึ้น
+            เริ่มต้นจากการเป็นส่วนหนึ่งในการออกแบบงบประมาณ
+            ผ่านการตรวจสอบและมีส่วนร่วมในการแสดงความคิดเห็นเสนอโครงการกับงบประมาณในการพัฒนาเมือง
+          </p>
+          <p>
+            เราเชื่อว่าอย่างน้อย...จุดเริ่มต้นนี้จะเป็นสิ่งผลักดันให้เกิดงบประมาณแบบมีส่วนร่วมอย่างแท้จริงในประเทศไทยได้!
+          </p>
+        </div>
+      </div>
+      <!-- data and restrictions -->
+      <div class="grid gap-6">
+        <h5 class="wv-h5 wv-bold">เกี่ยวกับโปรเจกต์นี้</h5>
+        <div class="grid gap-4">
+          <p>
+            ทางทีมผู้พัฒนา ได้ใช้ข้อมูลจากเว็บไซต์ทางการของกรุงเทพมหานครตามลิงก์ต่างๆ
+            ด้านล่าง อย่างไรก็ตาม
+            เนื่องจากโครงสร้างของข้อมูลไม่ได้จัดทำในรูปแบบเดียวกันอย่างเป็นระบบ
+            ไม่อัพเดท และไม่ครบถ้วน บางข้อมูลอยู่ในรูปแบบ pdf
+            ทำให้ต้องทำการเรียบเรียงและตรวจสอบจากข้อมูลหลายแห่ง อาจส่งผลข้อมูลได้
+            นอกจากนี้งบประมาณที่แสดงในเว็บไซต์ ไม่ใช่งบที่ทำการเบิกจ่ายใช้จริงๆ
+            เพราะข้อมูลไม่ครบถ้วน และสอบถามไปยังหน่วยงานแล้ว
+            ได้รับคำตอบว่าเป็นเรื่องเฉพาะภายในของเจ้าหน้าที่เท่านั้น
+            ซึ่งได้แจ้งเตือนในเว็บ
+            หากทางกรุงเทพมหานครมีการเปิดเผยข้อมูลจะนำมาอัพเดทให้ได้
+          </p>
+        </div>
+      </div>
+      <!-- refs -->
+      <div class="grid gap-6">
+        <h5 class="wv-h5 wv-bold">เกี่ยวกับโปรเจกต์นี้</h5>
+        <div class="grid gap-4">
+          <ul class="list-disc list-inside">
+            <li v-for="(ref, refIndex) in references" :key="refIndex">
+              {{ ref }}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- policy -->
+      <div class="grid gap-6">
+        <h5 class="wv-h5 wv-bold">เกี่ยวกับโปรเจกต์นี้</h5>
+        <div class="grid gap-4">
+          <p>
+            ทางทีมมีความตั้งใจที่พัฒนาทุกโปรเจ็กต์ให้เป็น Open Source และเปิดข้อมูลเป็น
+            Open Data ภายใต้เงื่อนไข Creative Commons Attribution-ShareAlike License
+            คือสามารถนำไปเผยแพร่และดัดแปลงได้ โดยต้องระบุที่มา แต่ห้ามนำไปใช้เพื่อการค้า
+            และต้องเผยแพร่งานดัดแปลงโดยใช้สัญญาอนุญาตชนิดเดียวกัน
+          </p>
+          <p>
+            หากมีข้อสงสัยต้องการสอบถามเพิ่มเติม
+            ประสงค์แจ้งเปลี่ยนแปลงหรือเพิ่มเติมข้อมูลเพื่อความถูกต้อง
+            หรือมีข้อเสนอแนะใดๆ สามารถติดต่อได้ที่ team [at] punchup.world
+          </p>
+        </div>
+      </div>
+      <!-- volunteers -->
+      <div class="grid gap-6">
+        <h5 class="wv-h5 wv-bold">อาสาสมัครร่วมพัฒนา</h5>
+        <div class="grid gap-8">
+          <div
+            v-for="({ position, names, text }, volunteerIndex) in volunteers"
+            :key="volunteerIndex"
+          >
+            <p class="wv-bold mb-2">{{ position }}</p>
+            <template v-for="(name, nameIndex) in names">
+              <span :key="nameIndex"
+                >{{ name }}{{ names?.length === nameIndex + 1 ? "" : "," }}
+              </span>
+            </template>
+            <div v-if="text">{{ text }}</div>
+          </div>
+        </div>
+      </div>
+      <!-- partners -->
+      <div class="grid gap-6">
+        <h5 class="wv-h5 wv-bold">เกี่ยวกับโปรเจกต์นี้</h5>
+        <div class="grid gap-4">
+          <ul class="list-disc list-inside">
+            <li
+              v-for="({ name }, partnerNameIndex) in partners"
+              :key="partnerNameIndex"
+            >
+              {{ name }}
+            </li>
+          </ul>
+        </div>
+        <div class="flex gap-2">
+          <div
+            v-for="({ name, img }, partnerImgIndex) in partners"
+            :key="partnerImgIndex"
+            class=""
+          >
+            <img
+              :src="require(`~/assets/images/partners/logo_${img}.png`)"
+              :alt="name"
+              class="w-full h-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </BoxContainer>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import BoxContainer from "~/components/BoxContainer.vue";
+
+export default Vue.extend({
+  name: "AboutPage",
+  components: { BoxContainer },
+  data() {
+    return {
+      references: [
+        "http://dp2.bangkok.go.th/mobile/mymenu.php",
+        "https://main.bangkok.go.th/",
+        "http://www.ratchakitcha.soc.go.th/RKJ/announce/search_result.jsp?openPageTime=1631042151747",
+        "https://www.citizenlab.co/blog/civic-engagement/participatory-budgeting/one-tool-multiple-possibilities-three-examples-of-participatory-budgets/",
+        "https://www.local.gov.uk/case-studies/govanhill-glasgow",
+        "https://www.local.gov.uk/case-studies/tower-hamlets-you-decide",
+        "https://www.local.gov.uk/case-studies/case-study-porto-alegre-brazil",
+        "ศูนย์สารสนเทศของกรุงเทพมหานคร",
+      ],
+      volunteers: [
+        {
+          position: "เขียนโปรแกรม",
+          names: ["พิมพ์ปฏิภาณ อังคเรืองรัตนา", "ธนภัทร จำนงรัตน์", "ณัฐกิตติ์ แซ่ตัน"],
+        },
+        {
+          position: "ออกแบบ",
+          names: ["นัฐพล ไก่แก้ว", "น้ำใส ศุภวงศ์"],
+        },
+        {
+          position: "สืบค้นและรวบรวมข้อมู",
+          names: ["ศุภิสรา อิศรานุกูล", "อดิศักดิ์ สายประเสริฐ"],
+        },
+        {
+          position: "บรรณาธิการ",
+          names: ["ธนิสรา เรืองเดช"],
+        },
+        {
+          position: "ประสานงานและจัดการอื่นๆ",
+          text: "Punch Up โปรเจกต์ Bangkok Budgeting ยังได้รับการสนับสนุนทุนในการดำเนินงานจาก National Endowment for Democracy (NED) และ Good Society ซึ่งนำมาใช้เป็นต้นทุนในการรวมรวมข้อมูล ออกแบบ พัฒนาเว็บไซต์ ประสานงาน บริหารจัดการ ตลอดจนการจัด Meetup เพื่อดำเนินโครงการ",
+        },
+        {
+          position: "ขอขอบคุณผู้ให้ข้อมูล",
+          text: "ผศ.ดร.ทวิดา กมลเวชช ที่ปรึกษาการพัฒนาการจัดทำแผนกรุงเทพมหานคร",
+        },
+      ],
+      partners: [
+        { name: "Good Society Thailand", img: "good_society" },
+        { name: "Glow Story", img: "glow" },
+        { name: "องค์กรต่อต้านคอร์รัปชัน (ประเทศไทย)", img: "act" },
+        { name: "Hand social enterprise", img: "hand" },
+        {
+          name: "Social Integrity Architecture and Mechanism Design Lab",
+          img: "siam_lab",
+        },
+      ],
+    };
+  },
+});
+</script>
