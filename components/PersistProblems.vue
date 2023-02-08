b8e52d0ed6c01d542afc6a64bd857d939a197588
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-09 02:35:40 +0700

    update persist problems layout - resize cards

--- components/PersistProblems.vue
+++ components/PersistProblems.vue
@@ -1,40 +1,42 @@
 <template>
   <BoxContainer>
-    <div class="wv-h8 sm:pb-12 text-center hidden sm:block">
+    <div class="wv-h5 font-extrabold pb-1 text-center hidden sm:block">
       {{ title }}
     </div>
-    <div class="wv-h5 sm:pb-12 text-center block sm:hidden wv-bold">
+    <div class="wv-h5 sm:pb-8 text-center block sm:hidden wv-bold">
       {{ title }}
     </div>
-    <!-- <p class="block sm:hidden text-center">เลือกได้ 1 ข้อ</p> -->
+    <p class="text-center">เลือกได้ 1 ข้อ</p>
     <!-- top -->
-    <div class="flex flex-wrap gap-2 md:gap-6 lg:gap-10 justify-center">
-      <div
-        v-for="(item, index) in problemsTop"
-        :key="index"
-        class="flex sm:flex-col bg-white sm:bg-inherit p-2 sm:p-0 rounded-md basis-full sm:basis-32 md:basis-40 flex-shrink-0 gap-4 sm:text-center items-center"
-      >
-        <img
-          class="w-10 h-10 sm:w-32 sm:h-32 md:w-40 md:h-40"
-          :src="`${$config.path.images}/persistent-problems/${item.img}.png`"
-          :alt="item.title"
-        />
-        <p class="wv-b4 wv-bold">{{ item.title }}</p>
+    <div class="flex flex-col gap-2 md:gap-5">
+      <div class="flex flex-wrap gap-2 md:gap-3 lg:gap-5 justify-center">
+        <div
+          v-for="(item, index) in problemsTop"
+          :key="index"
+          class="flex sm:flex-col bg-white rounded-md basis-full sm:basis-28 md:basis-40 flex-shrink-0 gap-2 sm:text-center items-center py-3 px-2 sm:py-5 sm:px-4"
+        >
+          <img
+            class="w-10 h-10 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:h-36 lg:w-36 max-w-none"
+            :src="`${$config.path.images}/persistent-problems/${item.img}.png`"
+            :alt="item.title"
+          />
+          <p class="wv-b4 wv-bold">{{ item.title }}</p>
+        </div>
       </div>
-    </div>
-    <!-- bottom -->
-    <div class="flex flex-wrap gap-2 md:gap-6 lg:gap-10 justify-center">
-      <div
-        v-for="(item, index) in problemsBottom"
-        :key="index"
-        class="flex sm:flex-col bg-white sm:bg-inherit p-2 sm:p-0 rounded-md basis-full sm:basis-32 md:basis-40 flex-shrink-0 gap-4 sm:text-center items-center"
-      >
-        <img
-          class="w-10 h-10 sm:w-32 sm:h-32 md:w-40 md:h-40"
-          :src="`${$config.path.images}/persistent-problems/${item.img}.png`"
-          :alt="item.title"
-        />
-        <p class="wv-b4 wv-bold">{{ item.title }}</p>
+      <!-- bottom -->
+      <div class="flex flex-wrap gap-2 md:gap-3 lg:gap-5 justify-center">
+        <div
+          v-for="(item, index) in problemsBottom"
+          :key="index"
+          class="flex sm:flex-col bg-white rounded-md basis-full sm:basis-28 md:basis-40 flex-shrink-0 gap-2 sm:text-center items-center py-3 px-2 sm:py-5 sm:px-4"
+        >
+          <img
+            class="w-10 h-10 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:h-36 lg:w-36 max-w-none"
+            :src="`${$config.path.images}/persistent-problems/${item.img}.png`"
+            :alt="item.title"
+          />
+          <p class="wv-b4 wv-bold">{{ item.title }}</p>
+        </div>
       </div>
     </div>
   </BoxContainer>
