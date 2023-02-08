a9825496323e1b1d5fc2f4b5575c92a47e9f3c12
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-09 02:14:24 +0700

    update mission statement

--- components/MissionStatement.vue
+++ components/MissionStatement.vue
@@ -1,22 +1,20 @@
 <template>
   <BoxContainer>
     <div class="w-full flex justify-center">
-      <div class="sm:max-w-lg">
-        <div class="rounded-lg sm:bg-black sm:text-white sm:py-14 sm:px-20 grid gap-6">
-          <div>
-            <p class="wv-b3 text-center">
-              ทุกวันนี้เมืองที่เราใช้ชีวิต
-              ยังมีหลายอย่างไม่ได้เป็นแบบที่เราวาดฝันไว้ใช่ไหม?
-            </p>
-            <p class="wv-b3 text-center">
-              เราเองในฐานะประชาชนผู้เสียภาษีทางตรงและทางอ้อม
-              ก็ควรได้ร่วมออกแบบการใช้งบประมาณของเมือง
-              เพื่อสร้างเมืองในแบบที่เราต้องการได้
-            </p>
-            <p class="wv-b3 text-center sm:text-wv-green wv-bold">
-              ถ้างั้นขอชวนมาลองเริ่มต้นที่เมืองหลวงอย่างกรุงเทพมหานครกันก่อนเลย!!
-            </p>
-          </div>
+      <div class="sm:max-w-2xl">
+        <div class="rounded-lg sm:py-10 grid gap-6">
+          <p class="wv-b3 text-center">
+            ทุกวันนี้เมืองที่เราใช้ชีวิต
+            ยังมีหลายอย่างไม่ได้เป็นแบบที่เราวาดฝันไว้ใช่ไหม?
+          </p>
+          <p class="wv-b3 text-center">
+            เราเองในฐานะประชาชนผู้เสียภาษีทางตรงและทางอ้อม
+            ก็ควรได้ร่วมออกแบบการใช้งบประมาณของเมือง
+            เพื่อสร้างเมืองในแบบที่เราต้องการได้
+          </p>
+          <p class="wv-b3 text-center wv-bold">
+            ถ้างั้นขอชวนมาลองเริ่มต้นที่เมืองหลวงอย่างกรุงเทพมหานครกันก่อนเลย!!
+          </p>
         </div>
       </div>
     </div>
