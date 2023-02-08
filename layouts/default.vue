4f30c0abbd91740e9ffe9aada132d32c8525f96b
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-09 01:31:17 +0700

    add cookie banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="min-h-screen wv-kondolar wv-b5">
+    <WvCookieConsent
+      policy-url="https://punchup.world"
+      :cookie-options="cookieOptions"
+      :on-accept="onCookieAccept"
+    />
     <WvNavbar class="z-20" :dark="true">
       <NuxtLink v-for="{ label, path } in routes" :key="path" :to="path">
         <WvNavButton :active="path === $route.path" :dark="true">{{
@@ -20,11 +25,20 @@ import WvNavbar from "@wevisdemo/ui/vue2/navbar";
 import WvFooter from "@wevisdemo/ui/vue2/footer";
 /* @ts-ignore */
 import WvNavButton from "@wevisdemo/ui/vue2/nav-button";
+/* @ts-ignore */
+import WvCookieConsent from "@wevisdemo/ui/vue2/cookie-consent";
 
 import Vue from "vue";
 import AboutFooter from "~/components/AboutFooter.vue";
+
+interface Option {
+  Functionality: boolean;
+  Performance: boolean;
+  Advertising: boolean;
+}
+
 export default Vue.extend({
-  components: { WvNavbar, WvFooter, WvNavButton, AboutFooter },
+  components: { WvNavbar, WvFooter, WvNavButton, WvCookieConsent, AboutFooter },
   data() {
     return {
       routes: [
@@ -34,12 +48,18 @@ export default Vue.extend({
         { label: "About", path: "/about" },
       ],
       footerComponent: "AboutFooter",
+      cookieOptions: ["Functionality"],
     };
   },
   methods: {
     isAboutFooter() {
       const routeName = this.$route.name;
       return routeName === "about";
+    },
+    onCookieAccept(option: Option) {
+      if (option.Functionality) {
+        // Initialize or enable analytics
+      }
     },
   },
 });
