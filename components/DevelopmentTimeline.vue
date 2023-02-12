3452f7857b1888d0dabf0cf8d95b5ac6ea6fbe8b
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-13 01:08:55 +0700

    update development timeline and responsive svg

--- components/DevelopmentTimeline.vue
+++ components/DevelopmentTimeline.vue
@@ -1,27 +1,41 @@
 <template>
   <BoxContainer>
-    <div class="text-center">
-      <p class="wv-b3">
-        ซึ่งเป็นแผนพัฒนาที่กรุงเทพฯ ตั้งเป้าหมายไว้ เพื่อจะก้าวไปสู่การเป็น
-      </p>
-      <p class="wv-h5 wv-bold">“มหานครแห่งเอเชีย”</p>
-      <p class="wv-b3">ภายในระยะเวลา 20 ปี (พ.ศ. 2561-2580)</p>
-    </div>
-    <div class="w-full">
-      <TimelineDesktop class="hidden sm:block" />
-      <div class="block sm:hidden">
-        <TimelineMobile />
-        <div class="grid gap-2 grid-cols-2 mt-4">
+    <div class="grid gap-8">
+      <div class="text-center">
+        <p class="wv-b3">
+          ซึ่งเป็นแผนพัฒนาที่กรุงเทพฯ ตั้งเป้าหมายไว้ เพื่อจะก้าวไปสู่การเป็น
+        </p>
+        <p class="wv-h5 wv-bold">“มหานครแห่งเอเชีย”</p>
+        <p class="wv-b3">ภายในระยะเวลา 20 ปี (พ.ศ. 2561-2580)</p>
+      </div>
+      <div class="w-full m-auto">
+        <div class="hidden sm:block">
           <div
-            v-for="({ year, text }, timelineIndex) in timeline"
-            :key="timelineIndex"
-            class="bg-white text-center"
+            class="flex justify-center h-0 w-full relative"
+            :style="`padding-top: ${paddingTopDesktop}%;`"
           >
-            <div class="flex gap-1 justify-center items-center">
-              <TimelinePointer />
-              <p class="wv-b6">{{ year }}</p>
+            <TimelineDesktop class="max-w-full absolute top-0" />
+          </div>
+        </div>
+        <div class="block sm:hidden">
+          <div
+            class="flex justify-center h-0 w-full relative"
+            :style="`padding-top: ${paddingTopMobile}%;`"
+          >
+            <TimelineMobile class="max-w-full absolute top-0" />
+          </div>
+          <div class="grid gap-2 grid-cols-2 mt-4">
+            <div
+              v-for="({ year, text }, timelineIndex) in timeline"
+              :key="timelineIndex"
+              class="bg-white text-center p-1"
+            >
+              <div class="flex gap-1 justify-center items-center">
+                <TimelinePointer />
+                <p class="wv-b6">{{ year }}</p>
+              </div>
+              <p v-for="(item, itemIndex) in text" :key="itemIndex">{{ item }}</p>
             </div>
-            <p>{{ text }}</p>
           </div>
         </div>
       </div>
@@ -41,7 +55,7 @@ import TimelinePointer from "~/assets/icons/timeline-pointer.svg?inline";
 
 interface Timeline {
   year: number;
-  text: string;
+  text: string[];
 }
 
 interface DevelopmentTimelineData {
@@ -56,16 +70,42 @@ export default Vue.extend({
       timeline: [
         {
           year: 2556,
-          text: "ออกแผนพัฒนากทม. 20 ปี ครั้งแรก",
+          text: ["ออกแผนพัฒนา", "กทม. 20 ปี ครั้งแรก"],
         },
         {
           year: 2561,
-          text: "ปรับปรุงแผนพัฒนากทม. 20 ปี ใหม่",
+          text: ["ปรับปรุงแผนพัฒนา", "กทม. 20 ปี ใหม่"],
         },
       ],
     };
   },
+  computed: {
+    paddingTopDesktop() {
+      return (193 / 924) * 100;
+    },
+    paddingTopMobile() {
+      return (76 / 300) * 100;
+    },
+  },
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.box-timeline-present,
+.arrow-timeline-present {
+  color: black;
+  animation: bgAnimate 1.5s ease-in-out infinite;
+}
+
+@keyframes bgAnimate {
+  0% {
+    color: rgba(0, 0, 0, 1);
+  }
+  50% {
+    color: rgba(0, 0, 0, 0.6);
+  }
+  100% {
+    color: rgba(0, 0, 0, 1);
+  }
+}
+</style>
