909bc69217ca0c42992a44d396ff7b9337048812
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-16 22:29:02 +0700

    update router config and remove setup pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ const config: NuxtConfig = {
   },
 
   router: {
-    base: BASE_PATH || "/",
+    base: process.env.NODE_ENV !== "production" ? BASE_PATH || "/" : "/bkk-budgeting/",
   },
 
   axios: {
