7c7e29a183710f8a3b47d3c5c25546457dbade83
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-19 03:25:05 +0700

    add cookie to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,6 +19,9 @@
 </template>
 
 <script lang="ts">
+import Vue from "vue";
+import { uuid } from "vue-uuid";
+
 /* @ts-ignore */
 import WvNavbar from "@wevisdemo/ui/vue2/navbar";
 /* @ts-ignore */
@@ -27,8 +30,6 @@ import WvFooter from "@wevisdemo/ui/vue2/footer";
 import WvNavButton from "@wevisdemo/ui/vue2/nav-button";
 /* @ts-ignore */
 import WvCookieConsent from "@wevisdemo/ui/vue2/cookie-consent";
-
-import Vue from "vue";
 import AboutFooter from "~/components/AboutFooter.vue";
 
 interface Option {
@@ -48,16 +49,48 @@ export default Vue.extend({
       ],
       footerComponent: "AboutFooter",
       cookieOptions: ["Functionality"],
+      cookies: false,
+      uuid: uuid.v1(),
     };
+  },
+  mounted() {
+    if (this.$cookies.get("uuid") === undefined) {
+      this.cookies = true;
+    } else {
+      this.cookies = false;
+    }
   },
   methods: {
     isAboutFooter() {
       const routeName = this.$route.name;
       return routeName === "about";
     },
-    onCookieAccept(option: Option) {
+    async onCookieAccept(option: Option) {
       if (option.Functionality) {
         // Initialize or enable analytics
+        this.$cookies.set("uuid", this.uuid);
+        this.$cookies.set("isVoted", false);
+        this.cookies = false;
+
+        const messageRef = this.$fire.database.ref("user");
+        const userSeq = this.$fire.database.ref("sequence").child("user_sequence");
+        try {
+          const a = await userSeq.once("value");
+          let r = a.val();
+          await messageRef.child((++r).toString()).set({
+            isInBkk: "",
+            hasHouseReg: "",
+            district: "",
+            province: "",
+            userid: this.$cookies.get("uuid"),
+          });
+          userSeq.set(r);
+        } catch (e) {
+          alert(e);
+          return;
+        }
+        // eslint-disable-next-line no-console
+        console.log("create user success");
       }
     },
   },
