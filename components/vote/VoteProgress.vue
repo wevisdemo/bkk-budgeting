4dc79e6358fcb4f1719e888f2e83c2b646e46ba9
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-18 18:32:30 +0700

    update district dropdown and vote progress styling

--- components/vote/VoteProgress.vue
+++ components/vote/VoteProgress.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="w-full h-10 relative">
+  <div class="w-full h-10 relative rounded border-2 border-white">
     <div class="flex gap-1 items-center z-20 h-full w-full relative py-4 px-2">
       <p class="wv-b3 flex-grow text-left">{{ strategyVote.title }}</p>
       <p class="wv-b4">({{ strategyVote.progress }}%)</p>
     </div>
     <div
-      :class="`top-0 left-0 z-10 absolute h-full ${bgColor}`"
+      :class="`top-0 left-0 z-10 absolute h-full ${
+        strategyVote.progress === 100 ? `rounded` : `rounded-l`
+      } ${bgColor}`"
       :style="`width: ${strategyVote.progress}%`"
     />
-    <div class="top-0 left-0 right-0 z-0 absolute h-full bg-wv-gray-4" />
+    <div class="top-0 left-0 right-0 z-0 absolute h-full" />
   </div>
 </template>
 
