5d0ee8b2b322a11adf2f6ba951c5aaed936eeee8
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-20 01:41:55 +0700

    update layout

--- components/GoodSociety.vue
+++ components/GoodSociety.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="bg-white">
     <BoxContainer>
-      <div class="sm:p-10 md:p-20 flex flex-col items-center gap-6">
-        <p class="wv-b3 text-center max-w-[650px]">
-          นี่เป็นเพียงจุดเริ่มต้นของการผลักดันไปสู่สังคมที่ดี (Good Society)
-          ที่ฝั่งประชาชนอย่างพวกเราจะเข้ามามีส่วนในการเปิดข้อมูลและแสดงความคิดเห็น
-          การวางแผนและการใช้งบประมาณของกรุงเทพมหานครได้
-        </p>
-        <img src="~/assets/images/good-society.jpg" />
+      <div class="flex flex-col items-center gap-6 text-center m-auto">
+        <h5 class="wv-h5 wv-bold max-w-[600px]">
+          ร่วมส่งต่อโครงการที่อยากให้เกิดขึ้นในพื้นที่ของคุณ
+        </h5>
+        <p class="wv-b6 text-wv-gray-1">ภาพปกเมื่อคุณแชร์เว็บไซต์นี้ :</p>
+        <div class="max-w-[1200px] border rounded outline">
+          <!-- implement image on district and strategy type -->
+          <img src="~/assets/images/good-society.jpg" />
+        </div>
+        <div>
+          <!-- add share social -->
+          <div>Share</div>
+          <div class="wv-b">
+            <span class="text-wv-gray-1">หรือร่วมกันติดแฮชแท็ก</span>
+            <span class="text-wv-green">#BangkokBudgeting</span>
+          </div>
+        </div>
       </div>
     </BoxContainer>
   </div>
