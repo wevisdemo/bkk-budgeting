6cb85107affe1970052dfe724d171933af84f286
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-02-23 23:01:49 +0700

    update question layout - use full image of circle and hand - absolutely center text in the circle

--- components/AllocateQuestion.vue
+++ components/AllocateQuestion.vue
@@ -1,36 +1,43 @@
 <template>
-  <div class="grid relative mb-[15rem] sm:mb-[18rem] justify-center">
-    <div
-      class="bg-wv-green rounded-full p-10 sm:p-20 flex flex-col items-center justify-center gap-4 aspect-square w-[112vw] h-[112vw] sm:w-[580px] sm:h-[580px] text-center z-30"
-    >
-      <p class="wv-b3">
-        ในปีต่อๆไป
-        <br />
-        กรุงเทพมหานครยังคงมุ่งพัฒนาและขับเคลื่อน
-        <span class="wv-bold">“ยุทธศาสตร์ทั้ง 7 ด้าน”</span>
-        ให้เกิดขึ้นต่อไป
-      </p>
-      <p class="wv-b3">
-        ถ้าบางส่วนของงบฯ ที่คุณมีส่วนช่วยตัดสินใจให้ถูกใช้อย่างตรงจุดได้
-      </p>
-      <p class="wv-b3 wv-bold">คุณคิดว่าควรนำไปพัฒนากับเรื่องอะไรบ้าง?</p>
+  <BoxContainer>
+    <div class="flex -mx-14 sm:m-auto relative my-4">
+      <div class="flex justify-center items-center w-full text-center">
+        <img
+          class="w-full h-full max-w-[600px]"
+          src="~/assets/images/question.png"
+          alt="proposed"
+        />
+        <div
+          class="absolute top-0 w-full max-w-[600px] max-h-[600px] rounded-full flex items-center justify-center aspect-square"
+        >
+          <div class="flex flex-col gap-4 px-16 sm:px-20 text-center">
+            <p class="wv-b3">
+              ในปีต่อๆไป
+              <br />
+              กรุงเทพมหานครยังคงมุ่งพัฒนาและขับเคลื่อน
+              <br />
+              <span class="wv-bold">“ยุทธศาสตร์ทั้ง 7 ด้าน”</span>
+              ให้เกิดขึ้นต่อไป
+            </p>
+            <p class="wv-b3">
+              ถ้าบางส่วนของงบฯ
+              <br />
+              ที่คุณมีส่วนช่วยตัดสินใจให้ถูกใช้อย่างตรงจุดได้
+            </p>
+            <p class="wv-b3 wv-bold">คุณคิดว่าควรนำไปพัฒนากับเรื่องอะไรบ้าง?</p>
+          </div>
+        </div>
+      </div>
     </div>
-    <div
-      class="flex justify-center absolute bottom-0 sm:bottom-[-50px] left-0 right-0 -mb-48 xs:-mb-52 sm:-mb-60"
-    >
-      <img
-        class="sm:max-w-xl h-auto"
-        :src="`${$config.path.images}/6_vote.png`"
-        alt="proposed"
-      />
-    </div>
-  </div>
+  </BoxContainer>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import BoxContainer from "./BoxContainer.vue";
 
 export default Vue.extend({
   name: "AllocationQuestion",
+  components: { BoxContainer },
 });
 </script>
