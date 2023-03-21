9ae703a5b0a94f415e1ee4eebcd56f60f968642b
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-03-22 01:44:04 +0700

    update project share

--- components/ProjectShare.vue
+++ components/ProjectShare.vue
@@ -7,25 +7,56 @@
         </h5>
         <div class="grid grid-cols-2 gap-2">
           <DistrictDropdown @change="district => onDistrictChange(district)" />
-          <ProjectDropdown
-            :disabled="isUpCountry"
-            @change="project => onProjectChange(project)"
-          />
+          <ProjectDropdown @change="project => onProjectChange(project)" />
         </div>
         <p class="wv-b6 text-wv-gray-1">ภาพปกเมื่อคุณแชร์เว็บไซต์นี้ :</p>
         <div class="ogimage max-w-[1000px] border rounded outline">
           <img
             :src="
               isUpCountry
-                ? `https://d208eq9ndr4893.cloudfront.net/og-image/upcountry.png`
-                : `https://d208eq9ndr4893.cloudfront.net/og-image/${district_name}-${project_id}.png`
+                ? `wevisdemo.github.io/bkk-budgeting/images/og-share/upcountry.png`
+                : `wevisdemo.github.io/bkk-budgeting/images/og-share/${project_id}-og-${project_name}/${district_name}-${project_id}.jpg`
             "
           />
         </div>
       </div>
       <div class="text-center">
-        <!-- add share social -->
-        <div>Share</div>
+        <div class="flex justify-center">
+          <div>Share</div>
+          <img
+            v-sharer
+            :src="icon_fb"
+            class="sharer social-icon pointer mx-1"
+            data-sharer="facebook"
+            :data-url="
+              district == 'upcountry'
+                ? `wevisdemo.github.io/bkk-budgeting/images/og-share/upcountry`
+                : `wevisdemo.github.io/bkk-budgeting/images/og-share/${project_id}-og-${project_name}/${district_name}-${project_id}.jpg`
+            "
+          />
+          <img
+            v-sharer
+            :src="icon_twitter"
+            class="sharer social-icon pointer mx-1"
+            data-sharer="twitter"
+            :data-url="
+              district == 'upcountry'
+                ? `wevisdemo.github.io/bkk-budgeting/images/og-share/upcountry`
+                : `wevisdemo.github.io/bkk-budgeting/images/og-share/${project_id}-og-${project_name}/${district_name}-${project_id}.jpg`
+            "
+          />
+          <img
+            v-sharer
+            :src="icon_line"
+            class="sharer social-icon pointer mx-1"
+            data-sharer="line"
+            :data-url="
+              district == 'upcountry'
+                ? `wevisdemo.github.io/bkk-budgeting/images/og-share/upcountry`
+                : `wevisdemo.github.io/bkk-budgeting/images/og-share/${project_id}-og-${project_name}/${district_name}-${project_id}.jpg`
+            "
+          />
+        </div>
         <div class="wv-b">
           <span class="text-wv-gray-1">หรือร่วมกันติดแฮชแท็ก</span>
           <span class="text-wv-green">#BangkokBudgeting</span>
@@ -37,6 +68,9 @@
 
 <script lang="ts">
 import Vue from "vue";
+// @ts-ignore
+import VueSharer from "vue-sharer";
+
 import BoxContainer from "./BoxContainer.vue";
 import DistrictDropdown, { District } from "~/components/DistrictDropdown.vue";
 import ProjectDropdown, { Project } from "~/components/ProjectDropdown.vue";
@@ -46,12 +80,19 @@ import districtData from "~/data/districts.json";
 export default Vue.extend({
   name: "ProjectShare",
   components: { BoxContainer, DistrictDropdown, ProjectDropdown },
+  directives: {
+    sharer: VueSharer,
+  },
   data() {
     return {
       district_name: districtData[4].en_name,
       project_id: 1,
+      project_name: "",
       isUpCountry: false,
       isDefault: true,
+      icon_fb: require("~/assets/logo/facebook.svg"),
+      icon_line: require("~/assets/logo/line.svg"),
+      icon_twitter: require("~/assets/logo/twitter.svg"),
     };
   },
   methods: {
@@ -73,6 +114,7 @@ export default Vue.extend({
     },
     onProjectChange(project: Project) {
       this.project_id = project.id;
+      this.project_name = project.name;
     },
   },
 });
