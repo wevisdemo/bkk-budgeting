ba38239edb3fb06c4864fafbed950b2124e97e4a
MheePooh <sophonjampasornklin@gmail.com>
2023-10-24 22:01:51 +0700

    yarn

--- nuxt.config.js
+++ nuxt.config.js
@@ -111,7 +111,7 @@ const config = {
 
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "/",
+    baseURL: "/bkk-budgeting",
   },
 
   pwa: {
